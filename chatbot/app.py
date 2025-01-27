1ae02b3021ab99c399f43cab9f117dc205f0a02e
AYANABHA SARKAR <ayanabhasarkar7998@gmail.com>
2025-01-27 20:07:32 +0530

    new change

---
+++ chatbot/app.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChaatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+import streamlit as st
+import os
+import dotenv import load_dotenv
+
+os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
